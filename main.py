f80c6e1e620e866b55c47d84fca786643ce5c001
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2021-11-30 21:57:27 +0200

    Basic tweets search

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import concurrent.futures
 import statistics
 
 import news
+import twitter
 import sentiment
 
 import time
@@ -19,11 +20,15 @@ def get_article_with_score(url):
     score = sentiment.vader_sentiment(article.text)
     return (article, score)
 
+def score_tweet(tweet):
+    score = sentiment.vader_sentiment(tweet.text)
+    return (tweet, score)
+
 
 @app.route('/')
 def index():
     return render_template('index.html',
-            search_type='twitter')
+            search_type='tweets')
 
 
 @app.route('/search-news', methods=['POST'])
@@ -69,6 +74,42 @@ def search_news():
     return render_template('index.html',
             search_type='news',
             articles=articles,
+            form=request.form,
+            sentiment_scores=sentiment_scores,
+            mean_sentiment_score=mean_score,
+            median_sentiment_score=median_score,
+            std_dev_sentiment_scores=std_dev
+            )
+
+@app.route('/search-tweets', methods=['POST'])
+def search_tweets():
+    print(request.form)
+    query = request.form.get('query')
+
+    tweets = twitter.search_tweets(query, limit=250)
+    sentiment_scores = {}
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+
+        for tweet in tweets:
+            futures.append(executor.submit(score_tweet, tweet))
+
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                tweet, score = future.result()
+                sentiment_scores[tweet.id] = score
+            except:
+                # TODO
+                pass
+
+    mean_score = statistics.mean(sentiment_scores.values())
+    median_score = statistics.median(sentiment_scores.values())
+    std_dev = statistics.stdev(sentiment_scores.values()) if len(sentiment_scores) > 1 else None
+
+    return render_template('index.html',
+            search_type='tweets',
+            tweets=tweets,
             form=request.form,
             sentiment_scores=sentiment_scores,
             mean_sentiment_score=mean_score,
--- twitter.py
+++ twitter.py
@@ -6,16 +6,32 @@ client = tweepy.Client(
         consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
         bearer_token=os.environ['TWITTER_BEARER_TOKEN'])
 
-tweets = []
+# tweets = []
 
-for tweet in tweepy.Paginator(client.search_recent_tweets,
-        'cop26 -is:retweet lang:en',
-        max_results=10,
-        ).flatten(30):
-    print(len(tweet.text))
-    print(tweet.text)
-    print('===')
-    tweets.append(tweet)
+# for tweet in tweepy.Paginator(
+#         client.search_recent_tweets,
+#         'cop26 -is:retweet lang:en',
+#         max_results=10,
+#         tweet_fields=['id', 'text', 'created_at']
+#         ).flatten(limit=10):
+#     print(tweet.id)
+#     print(tweet.created_at)
+#     print(dir(tweet))
+#     print(len(tweet.text))
+#     print(tweet.text)
+#     print('===')
+#     tweets.append(tweet)
+
 
 # print([t.text for t in tweets])
+
+def search_tweets(query, limit=250):
+    final_query = query + ' -is:retweet lang:en'
+    tweets = tweepy.Paginator(
+            client.search_recent_tweets,
+            final_query,
+            max_results=100,
+            tweet_fields=['id', 'text', 'created_at']
+            ).flatten(limit=limit)
+    return list(tweets)
 
