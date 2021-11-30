be35f0f0de9ae39214c072508eeb60a900bac7d1
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2021-11-30 22:07:26 +0200

    Minor improvements in the tweets search UI

--- twitter.py
+++ twitter.py
@@ -6,24 +6,6 @@ client = tweepy.Client(
         consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
         bearer_token=os.environ['TWITTER_BEARER_TOKEN'])
 
-# tweets = []
-
-# for tweet in tweepy.Paginator(
-#         client.search_recent_tweets,
-#         'cop26 -is:retweet lang:en',
-#         max_results=10,
-#         tweet_fields=['id', 'text', 'created_at']
-#         ).flatten(limit=10):
-#     print(tweet.id)
-#     print(tweet.created_at)
-#     print(dir(tweet))
-#     print(len(tweet.text))
-#     print(tweet.text)
-#     print('===')
-#     tweets.append(tweet)
-
-
-# print([t.text for t in tweets])
 
 def search_tweets(query, limit=250):
     final_query = query + ' -is:retweet lang:en'
